7f8e46c33446f70b0547bc2bf313eaedeab4f665
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-06-22 04:28:54 +0200

    update

--- home.php
+++ home.php
@@ -6,67 +6,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap"
+        rel="stylesheet">
     <style>
-        body{
+        body {
             background-color: whitesmoke;
             font-family: "Tajawal", sans-serif;
         }
-       #mother{
-        width: 100%;
-        font-size: 20px;
-       }
-       main{
-        float: left;
-        border: 1px solid gray;
-        padding: 5px;
-       }
-       input{
-        padding: 4px;
-        border: 2px solid black;
-        text-align: center;
-        font-size: 17px;
-        font-family: "Tajawal", sans-serif;
-       }
-       aside{
-        text-align: center;
-        width: 300px;
-        float: right;
-        border: 1px solid black;
-        padding: 10px;
-        background-color:silver ;
-        color: white;
-       }
-       #tbl{
-        width: 890px;
-font-size: 20px;
-       }
-       #tbl th {
-         background-color: silver;
-         color: #000;
-         font-size: 20px;
-         padding: 10px;
-       }
-       aside button{
-        width: 190px;
-        padding: 8px;
-        margin-top: 7px;
-        font-size: 17px;
-        font-family: "Tajawal", sans-serif;
-        font-weight:bold ;
 
-       }
+        #mother {
+            width: 100%;
+            font-size: 20px;
+        }
+
+        main {
+            float: left;
+            border: 1px solid gray;
+            padding: 5px;
+        }
+
+        input {
+            padding: 4px;
+            border: 2px solid black;
+            text-align: center;
+            font-size: 17px;
+            font-family: "Tajawal", sans-serif;
+        }
+
+        aside {
+            text-align: center;
+            width: 300px;
+            float: right;
+            border: 1px solid black;
+            padding: 10px;
+            background-color: silver;
+            color: white;
+        }
+
+        #tbl {
+            width: 890px;
+            font-size: 20px;
+        }
+
+        #tbl th {
+            background-color: silver;
+            color: #000;
+            font-size: 20px;
+            padding: 10px;
+        }
+
+        aside button {
+            width: 190px;
+            padding: 8px;
+            margin-top: 7px;
+            font-size: 17px;
+            font-family: "Tajawal", sans-serif;
+            font-weight: bold;
+
+        }
     </style>
 </head>
 
 <body dir="rtl">
+
+    
     <div id='mother'>
 
         <form method="post">
             <aside>
                 <div>
-                    <img src="logo.png" alt="لوجو الموقع" width="200px">
+                    <img src="logo.png" alt="لوجو الموقع" width="200px" >
 
                     <h3>لوحة المدير</h3>
 
