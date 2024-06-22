2ce7ec6c56cfbe7a535ed8fa42cdc396a09f974f
SaadBarhrouj <saad.barhrouj@etu.uae.ac.ma>
2024-06-22 05:26:14 +0200

    update

--- home.php
+++ home.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Gestion des Étudiants</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap"
-        rel="stylesheet">
+          rel="stylesheet">
     <style>
         body {
             background-color: whitesmoke;
@@ -54,7 +53,19 @@
             color: #000;
             font-size: 20px;
             padding: 10px;
+
         }
+        #tbl td  {
+            border:1px solid black;
+            color: #000;
+            font-size: 20px;
+            padding: 10px;
+            text-align: center;
+            background-color: whitesmoke;
+
+        }
+
+        
 
         aside button {
             width: 190px;
@@ -63,104 +74,101 @@
             font-size: 17px;
             font-family: "Tajawal", sans-serif;
             font-weight: bold;
-
         }
     </style>
 </head>
-
 <body dir="rtl">
-       <?php 
-     
-       $host='localhost';
-       $user='root';
-       $pass='';
-       $db='students';
-       $con=mysqli_connect($host,$user,$pass,$db);
-       $res=mysqli_query($con,"select * from student");
-       $id='';
-       $name='';
-       $address='';
+<?php 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'students';
 
-       if(isset($_POST['id'])){
-        $id=$_POST['id'];
-       }
+// Connexion à la base de données
+$con = mysqli_connect($host, $user, $pass, $db);
+if (!$con) {
+    die("Échec de la connexion : " . mysqli_connect_error());
+}
 
-       if(isset($_POST['name'])){
-        $id=$_POST['name'];
-       }
-       
-       if(isset($_POST['address'])){
-        $id=$_POST['address'];
-       }
-       $sqls='';
-       if (isset($_POST['add'])){
-      $sqls="insert into student value($id,'$name','$address')";
-       }
-       $sqls='';
-       if (isset($_POST['add'])){
-      $sqls="insert into student value($id,'$name','$address')";
-      mysqli_query($con,$sqls);
-       header("location:home.php");
-       }
-       
-       if (isset($_POST['del'])){
-        $sqls="delete from student where name ='$name'";
-        mysqli_query($con,$sqls);
-        header("location:home.php");
-         }
-        
-       ?>
-    
-    <div id='mother'>
+// Récupération des données des étudiants
+$res = mysqli_query($con, "SELECT * FROM student");
+if (!$res) {
+    die("Erreur de requête : " . mysqli_error($con));
+}
 
-        <form method="post">
-            <aside>
-                <div>
-                    <img src="logo.png" alt="لوجو الموقع" width="200px" >
+$id = '';
+$name = '';
+$address = '';
 
-                    <h3>لوحة المدير</h3>
+// Récupération des données POST
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+}
 
-                    <label>رقم الطالب</label><br>
-                    <input type="text" name="id" id="id"><br>
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+}
 
-                    <label>اسم الطالب</label><br>
-                    <input type="text" name="name" id="name"><br>
+if (isset($_POST['address'])) {
+    $address = $_POST['address'];
+}
 
-                    <label>عنوان الطالب</label><br>
-                    <input type="text" name="address" id="address"><br><br>
+// Ajout d'un étudiant
+if (isset($_POST['add'])) {
+    $sqls = "INSERT INTO student (id, name, address) VALUES ('$id', '$name', '$address')";
+    if (mysqli_query($con, $sqls)) {
+        header("Location: home.php");
+    } else {
+        die("Erreur d'insertion : " . mysqli_error($con));
+    }
+}
 
-                    <button name="add">إضافة طالب</button>
+// Suppression d'un étudiant
+if (isset($_POST['del'])) {
+    $sqls = "DELETE FROM student WHERE name = '$name'";
+    if (mysqli_query($con, $sqls)) {
+        header("Location: home.php");
+    } else {
+        die("Erreur de suppression : " . mysqli_error($con));
+    }
+}
+?>
 
-                    <button name="del"> حذف طالب</button>
-
-                </div>
-            </aside>
-
-
-
-            <main>
-                <table id="tbl">
-                    <tr>
-                        <th>الرقم التسلسلي</th>
-                        <th>اسم الطالب</th>
-                        <th>عنوان الطالب</th>
-                    </tr>
-                     <?php
-                     while($row=mysqli_fetch_array($res)){
-                        echo "<tr>";
-                        echo "<td>".$row['id']."<td>";
-                        echo "<td>".$row['name']."<td>";
-                        echo "<td>".$row['address']."<td>";
-                        echo "</tr>";
-                     }
-
-                     ?> 
-
-                </table>
-            </main>
-
-        </form>
-    </div>
+<div id='mother'>
+    <form method="post">
+        <aside>
+            <div>
+                <img src="logo.png" alt="لوجو الموقع" width="200px">
+                <h3>لوحة المدير</h3>
+                <label>رقم الطالب</label><br>
+                <input type="text" name="id" id="id"><br>
+                <label>اسم الطالب</label><br>
+                <input type="text" name="name" id="name"><br>
+                <label>عنوان الطالب</label><br>
+                <input type="text" name="address" id="address"><br><br>
+                <button name="add">إضافة طالب</button>
+                <button name="del"> حذف طالب</button>
+            </div>
+        </aside>
+        <main>
+            <table id="tbl">
+                <tr>
+                    <th>الرقم التسلسلي</th>
+                    <th>اسم الطالب</th>
+                    <th>عنوان الطالب</th>
+                </tr>
+                <?php
+                while($row = mysqli_fetch_array($res)){
+                    echo "<tr>";
+                    echo "<td>".$row['id']."</td>";
+                    echo "<td>".$row['name']."</td>";
+                    echo "<td>".$row['address']."</td>";
+                    echo "</tr>";
+                }
+                ?> 
+            </table>
+        </main>
+    </form>
+</div>
 </body>
-
 </html>
